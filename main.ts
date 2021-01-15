407bdcff68aeb526ba4842c5c3d2552caecc76e7
Rachel Ramsay <rachel@infowest.com>
2021-01-15 14:12:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.showString("Hello World")
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+})
