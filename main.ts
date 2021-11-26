1f085050b0ddcbfb3cba20190f72f1eefa8be505
Rachel Ramsay <ramsay@dixie.edu>
2021-11-25 23:26:30 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    basic.showString("X" + input.acceleration(Dimension.X))
+    basic.showString("Y" + input.acceleration(Dimension.Y))
+    basic.showString("Z" + input.acceleration(Dimension.Z))
+})
